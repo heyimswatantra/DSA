9164a6f1887a8e9e716a17e1525a3ad6972c3706
swatantra1265 <swatantra.s.yadav@slrtce.in>
2023-06-11 21:54:12 +0530

    84. Largest Rectangle in Histogram

---
+++ LEETCODE/largestRectangleInHistogram.java
@@ -0,0 +1,69 @@
+package LEETCODE;
+
+import java.util.Map;
+import java.util.Stack;
+
+public class largestRectangleInHistogram {
+    //https://leetcode.com/problems/largest-rectangle-in-histogram/
+
+    //better approach
+    public static int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Stack<Integer> st = new Stack<>();
+        int[] leftSmall = new int[n];
+        int[] rightSmall = new int[n];
+
+        //compute left just smaller element than current element
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]){
+                st.pop();
+            }
+            if(st.isEmpty()) leftSmall[i] = 0;
+            else leftSmall[i] = st.peek() + 1;
+            st.push(i);
+        }
+
+        //empty the stack for re-use
+        while (!st.isEmpty()) st.pop();
+
+        //compute right just smaller element than current element
+        for (int i = n-1; i >= 0; i--) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]){
+                st.pop();
+            }
+            if(st.isEmpty()) rightSmall[i] = n-1;
+            else rightSmall[i] = st.peek() -1;
+            st.push(i);
+        }
+
+        int maxiA = 0;
+        //compute area by width(rightSmall -leftSmall) * height
+        for (int i = 0; i < n; i++) {
+            maxiA = Math.max(maxiA, (rightSmall[i] - leftSmall[i] + 1) * heights[i]);
+        }
+        return maxiA;
+    }
+
+    //optimal approach
+    public static int largestRectArea(int[] heights){
+        int maxA = 0;
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i <= heights.length; i++){
+            while (!st.isEmpty() && (i == heights.length || heights[st.peek()] >= heights[i])) {
+                int length = heights[st.peek()];
+                st.pop();
+                int width = 0;
+                if(st.isEmpty()) width = i;
+                else width = i - st.peek() - 1;
+                maxA = Math.max(maxA, length * width);
+            }
+            st.push(i);
+        }
+        return maxA;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,1,5,6,2,3};
+        System.out.println(largestRectangleArea(arr));
+    }
+}
--- LEETCODE/nextGreaterElement.java
+++ LEETCODE/nextGreaterElement.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Stack;
 
+//https://leetcode.com/problems/next-greater-element-i/
 public class nextGreaterElement {
 
     public static int[] nxtGrtEle(int[] nums1, int[] nums2) {
+
         //initialize an empty result array with all vals as -1 of size equal to nums1 arr
         int[] result = new int[nums1.length];
         Arrays.fill(result, -1);
@@ -21,13 +23,14 @@ public class nextGreaterElement {
 
         //iterate through nums2
         for (int curr : nums2) {
-            
+
             //if stack is not empty and current element of nums2 is greater than stack top,
             //then pop the stack top element, get index of it in nums1 array
             //and add current element at that index in result array
             while (!st.isEmpty() && curr > st.peek()) {
                 int val = st.pop();
-                int indx = hm.getOrDefault(val,0);
+                System.out.println(hm);
+                int indx = hm.getOrDefault( val,0);
                 result[indx] = curr;
             }
 
@@ -37,12 +40,25 @@ public class nextGreaterElement {
             }
         }
         return result;
+
+        /*
+        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }
+        for (int i = 0; i < nums1.length; i++)
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        return nums1;
+        */
     }
 
     public static void main(String[] args) {
 
-        int[] nums1 = {2,4};
-        int[] nums2 = {1,2,3,4};
+        int[] nums1 = {2,1,3};
+        int[] nums2 = {2,3,1};
 
         int[] rs = nxtGrtEle(nums1, nums2);
         for (int r : rs) {
